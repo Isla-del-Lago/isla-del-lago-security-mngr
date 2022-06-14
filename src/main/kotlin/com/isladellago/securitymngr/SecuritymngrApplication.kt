d46c9db114366dd04d08f8772b2e27cfc4c42a8c
Camilo Lopez <camilino123@gmail.com>
2022-06-14 15:41:40 -0500

    Project init

---
+++ src/main/kotlin/com/isladellago/securitymngr/SecuritymngrApplication.kt
@@ -0,0 +1,11 @@
+package com.isladellago.securitymngr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecuritymngrApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecuritymngrApplication>(*args)
+}
---
+++ src/test/kotlin/com/isladellago/securitymngr/SecuritymngrApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.isladellago.securitymngr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecuritymngrApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
